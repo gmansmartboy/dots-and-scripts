5f6e93ef2a8526eb1b903c6ac2846dd3ef58f282
gmansmartboy <88818390+gmansmartboy@users.noreply.github.com>
2023-12-14 23:02:47 -0800

    Update install.sh
    Added a few more packages

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y git zsh ktouch gtypist screenfetch neofetch
+sudo apt-get install -y git zsh ktouch gtypist neofetch tty-clock neovim unzip python3 php mysql apache2 
